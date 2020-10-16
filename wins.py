b6a1e77073be5d4f2e2ae605b98b6b43f0c06793
drake1905 <53058032+drake1905@users.noreply.github.com>
2020-10-16 17:35:23 -0400

    Update wins.py

--- wins.py
+++ wins.py
@@ -1,25 +1,21 @@
 import random
 
 
-class Win_Calc:
+def is_player_good(winlist):
 
-    def is_player_good(self, winlist):
+    if winlist < 0.33:
+        message = ['DIS MANE STINKS', 'run while you can', 'I repeat, YOU ARE NOT WINNING THIS', 'I predict a fat L', 'Have fun trying to carry this person', 'He is a walking trash can', 'He needs to find a new game', 'BAD LUCK!!!']
+        return random.choice(message)
 
+    elif 0.33 < winlist <= 0.5:
+        message = ['Losing a bit', 'Not very good', 'He needs lots of help', 'Your back might hurt a little', 'Does not win much']
+        return random.choice(message)
 
-        if winlist < 0.33:
-            trash = ['DIS MANE STINKS', 'run while you can', 'I repeat, YOU ARE NOT WINNING THIS', 'I predict a fat L', 'Have fun trying to carry this person', 'He is a walking trash can', 'He needs to find a new game', 'BAD LUCK!!!']
-            return (random.choice(trash))
+    elif 0.5 < winlist <= 0.65:
+        message = ['He is ight', 'He can win a lil', 'You guys have a decent chance to win', 'Serviceable', 'Should be a dub']
+        return random.choice(message)
 
-        elif winlist > 0.33 and winlist <= 0.5:
-            notgood = ['Losing a bit', 'Not very good', 'He needs lots of help', 'Your back might hurt a little', 'Does not win much']
-            return (random.choice(notgood))
-
-        elif winlist > 0.5 and winlist <= 0.65:
-            ight = ['He is ight', 'He can win a lil', 'You guys have a decent chance to win', 'Serviceable', 'Should be a dub']
-            return (random.choice(ight))
-
-        elif winlist > 0.65:
-            good = ['DUB!', 'You getting carried', 'His back gonna hurt a bit', 'winner winner chicken dinner', 'Dude wins TOO MUCH', 'You aint even gotta try', 'GODLIKE']
-            return (random.choice(good))
-
+    elif winlist > 0.65:
+        message = ['DUB!', 'You getting carried', 'His back gonna hurt a bit', 'winner winner chicken dinner', 'Dude wins TOO MUCH', 'You aint even gotta try', 'GODLIKE']
+        return random.choice(message)
 
