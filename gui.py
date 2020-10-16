64c0536fbb6fe9f64526befa0de7e2f8f73a89a2
drake1905 <53058032+drake1905@users.noreply.github.com>
2020-10-16 17:34:47 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 from tkinter import font as tkfont
-from getId import Id_Collect
+from getId import id_collected
 from games import Game
-from wins import Win_Calc
+from wins import is_player_good
 
 
 class RiotApp(tk.Tk):
@@ -26,18 +26,27 @@ class RiotApp(tk.Tk):
     def show_frame(self, page_name):
         frame = self.frames[page_name]
         frame.tkraise()
+    
+    def collect_data(self, name, key):#used to be a different class if u want to change it back
+        num_games = 20
+        game = Game()
+        accId = id_collected(name, key)
+        game_list = game.find_game_ids(accId, key, num_games)
+        global stat_list
+        stat_list = game.game_data(game_list, key, name, num_games)
+        global honest
+        honest = is_player_good(stat_list[5])
 
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
     
         tk.Frame.__init__(self, parent)
-        c = DataCollected()
         self.controller = controller
         self.label = tk.Label(self, text="Enter summoner name:", width = 20, font = ("bold", 20))
         self.label.place(x=90,y=53)
-        self.entry1 = tk.Entry(self)
-        self.entry1.place(x=190,y=130)
+        self.entry = tk.Entry(self)
+        self.entry.place(x=190,y=130)
         
         self.button = tk.Button(self, text="Search",width = 20, bg = 'brown', fg = 'white',
                             command=lambda: data_collected(self,controller))
@@ -45,31 +54,16 @@ class StartPage(tk.Frame):
 
         def data_collected(self,controller):
         
-            name = self.entry1.get()
-            Key = 'RGAPI-446824c1-a469-4c6c-95ca-4a5b911ba745'
-            ids = Id_Collect()
-            a = ids.id_collected(name, Key)
+            name = self.entry.get()
+            key = 'RGAPI-6112b999-ed88-4cec-82f4-a4965fce6c28'
+            a = id_collected(name, key)
             if a != 'NO':
                 controller.show_frame("MenuPage")
-                stat_list = c.collect_data(name, Key)
+                controller.collect_data(name, key)
             else:
                 controller.show_frame('StartPage')
 
-class DataCollected():
-    
-    def collect_data(self, name, Key):
-
-        num_games = 20
-        ids = Id_Collect()
-        game = Game()
-        wins = Win_Calc()
-        accId = ids.id_collected(name, Key)
-        game_list = game.find_game_ids(accId, Key, num_games)
-        global stat_list
-        stat_list = game.game_data(game_list, Key, name, num_games)
-        return stat_list
-
-class MenuPage(tk.Frame,DataCollected):
+class MenuPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -77,7 +71,7 @@ class MenuPage(tk.Frame,DataCollected):
         label = tk.Label(self, text="Main Menu", font=controller.title_font)
         label.place(x=180,y=50)
         button = tk.Button(self, text="Kill Average",width = 20, bg = 'brown', fg = 'white',
-                           command=lambda: [controller.show_frame("KillPage"), change_label]).place(x=180,y=100)
+                           command=lambda: controller.show_frame("KillPage")).place(x=180,y=100)
         button = tk.Button(self, text="Death Average",width = 20, bg = 'brown', fg = 'white',
                            command=lambda: controller.show_frame("DeathPage")).place(x=180,y=150)
         button = tk.Button(self, text="Cs Average",width = 20, bg = 'brown', fg = 'white',
@@ -86,10 +80,8 @@ class MenuPage(tk.Frame,DataCollected):
                            command=lambda: controller.show_frame("HonestPage")).place(x=180,y=250)
         button = tk.Button(self, text="Back",width = 20, bg = 'brown', fg = 'white',
                            command=lambda: controller.show_frame("StartPage")).place(x=180,y=300)
-        def change_label():
-            self.label1.configure(text=stat_list[0])
 
-class KillPage(tk.Frame, DataCollected):
+class KillPage(tk.Frame):
     
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
@@ -101,6 +93,7 @@ class KillPage(tk.Frame, DataCollected):
         self.label1.after(1000, self.refresh_label)
         self.button = tk.Button(self, text = "Back", width = 20, bg = 'brown', fg = 'white',
                         command=lambda: controller.show_frame("MenuPage")).place(x=180,y=300)
+
     def refresh_label(self):
         self.label1.configure(text = stat_list[1])
         self.label1.after(1000,self.refresh_label)
@@ -117,6 +110,7 @@ class DeathPage(tk.Frame):
         self.label2.after(1000, self.refresh_label)
         self.button = tk.Button(self, text="Back", width = 20, bg = 'brown', fg = 'white',
                            command=lambda: controller.show_frame("MenuPage")).place(x=180,y=300)
+
     def refresh_label(self):
         self.label2.configure(text = stat_list[0])
         self.label2.after(1000,self.refresh_label)
@@ -133,6 +127,7 @@ class CsPage(tk.Frame):
         self.label3.after(1000, self.refresh_label)
         self.button = tk.Button(self, text="Back", width = 20, bg = 'brown', fg = 'white',
                            command=lambda: controller.show_frame("MenuPage")).place(x=180,y=300)
+
     def refresh_label(self):
         self.label3.configure(text = stat_list[4])
         self.label3.after(1000,self.refresh_label)
@@ -140,25 +135,24 @@ class CsPage(tk.Frame):
 class HonestPage(tk.Frame):
 
     def __init__(self, parent, controller):
-        wins = Win_Calc()
-        self.honest = wins.is_player_good(stat_list[5])
         tk.Frame.__init__(self, parent)
         self.controller = controller
         self.label = tk.Label(self, text = 'Honest Truth', width=20,font=("bold", 20))
         self.label.place(x=90, y=100)
         self.label4 = tk.Label(self, text = " ", width=20,font=("bold", 20))
         self.label4.place(x=90,y=150)
-        self.label4.after(1000, self.refresh_label(self.honest))
+        self.label4.after(1000, self.refresh_label())
         self.button = tk.Button(self, text = "Back", width = 20, bg = 'brown', fg = 'white',
                            command=lambda: controller.show_frame("MenuPage")).place(x=180,y=300)
-    def refresh_label(self, honest):
+
+    def refresh_label(self):
         self.label4.configure(text = honest)
         self.label4.after(1000,self.refresh_label)
-
 
 if __name__ == "__main__":
 
     stat_list = [1,1,1,1,1,1,1]
+    honest = ' '
     root = RiotApp()
     root.geometry("500x500")
     root.mainloop()
