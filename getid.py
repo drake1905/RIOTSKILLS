84e01805bf73077c50305937cec7dc853c74f661
drake1905 <53058032+drake1905@users.noreply.github.com>
2020-12-17 01:46:37 -0500

    Update getid.py

--- getid.py
+++ getid.py
@@ -4,7 +4,6 @@ def id_collected(sumName, key):
     # COLLECTING DATA TO BE INSERTING FOR MATCHLIST DATABASE
     url = ('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+(sumName)+'?api_key='+(key))
     response = requests.get(url)
-    print (response)
     if response.status_code == 200:
         accId = (response.json()['accountId'])
         return accId
